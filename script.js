ee5d6345660e45d48a11a217855797b5c25f134c
PRISCILA RUOTOLO <126161177+PRISCILARUOTOLO@users.noreply.github.com>
2026-06-10 17:00:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function respostas(status){
 
-    if (status === 'correta'){
+    if (status ==='correta'){
         alert("Sim, a professora nasceu na cidade de Alvorada do Sul")
     } else{
         alert("Você errou, descubra mais informações.")
